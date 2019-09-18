abf6aacfe3dfe48971bad4ae4ba40efc17f6607c
Jimmy Tang <jimmytangdev@gmail.com>
2019-09-18 00:08:35 -0400

    Initial commit

---
+++ app/src/main/java/com/test/simplechat/Register.java
@@ -0,0 +1,125 @@
+package com.test.simplechat;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.firebase.client.Firebase;
+import com.google.firebase.analytics.FirebaseAnalytics;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Register extends AppCompatActivity {
+
+    private FirebaseAnalytics mFirebaseAnalytics;
+    EditText username, password;
+    Button registerButton;
+    String user, pass;
+    TextView login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+
+        username = (EditText)findViewById(R.id.username);
+        password = (EditText)findViewById(R.id.password);
+        registerButton = (Button)findViewById(R.id.registerButton);
+        login = (TextView)findViewById(R.id.login);
+
+        Firebase.setAndroidContext(this);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Register.this, Login.class));
+            }
+        });
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                user = username.getText().toString();
+                pass = password.getText().toString();
+
+                if(user.equals("")){
+                    username.setError("can't be blank");
+                }
+                else if(pass.equals("")){
+                    password.setError("can't be blank");
+                }
+                else if(!user.matches("[A-Za-z0-9]+")){
+                    username.setError("only alphabet or number allowed");
+                }
+                else if(user.length()<5){
+                    username.setError("at least 5 characters long");
+                }
+                else if(pass.length()<5){
+                    password.setError("at least 5 characters long");
+                }
+                else {
+
+                    String url = "https://simplechat-208c9.firebaseio.com/users.json";
+
+                    StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>(){
+                        @Override
+                        public void onResponse(String s) {
+                            Firebase reference = new Firebase("https://simplechat-208c9.firebaseio.com/users");
+
+                            if(s.equals("null")) {
+                                reference.child(user).child("password").setValue(pass);
+                                Toast.makeText(Register.this, "registration successful", Toast.LENGTH_LONG).show();
+                            }
+                            else {
+                                try {
+                                    JSONObject obj = new JSONObject(s);
+
+                                    if (!obj.has(user)) {
+                                        reference.child(user).child("password").setValue(pass);
+                                        Toast.makeText(Register.this, "registration successful", Toast.LENGTH_LONG).show();
+                                        Bundle bundle = new Bundle();
+                                        bundle.putString(FirebaseAnalytics.Param.ITEM_ID, "1");
+                                        bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, "registered");
+                                        mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle);
+                                        finish();
+                                    } else {
+                                        Toast.makeText(Register.this, "username already exists", Toast.LENGTH_LONG).show();
+                                    }
+
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+
+
+                        }
+
+                    },new Response.ErrorListener(){
+                        @Override
+                        public void onErrorResponse(VolleyError volleyError) {
+                            System.out.println("" + volleyError );
+
+                        }
+                    });
+
+                    RequestQueue rQueue = Volley.newRequestQueue(Register.this);
+                    rQueue.add(request);
+                }
+            }
+        });
+    }
+}
